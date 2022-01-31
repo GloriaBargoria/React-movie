7b0d7de5d8f6cd07c41bdadecf0fd610c74fda64
GloriaBargoria <bargoriagloria@gmail.com>
2022-01-31 19:01:15 +0300

    add to favorites feature added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddToFavorites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState(''); //state value to store what the user types
+    const [favorites, setFavorites] = useState([]);
 
-    const getMovieRequest = async () => {
-        const url = 'http://www.omdbapi.com/?s=star wars&apikey=82f1a1ee';
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=82f1a1ee`;
 
         const response = await fetch(url);
         const responseJson = await response.json();
@@ -17,16 +22,31 @@ const App = () => {
         }
     };
 
+    const AddFavoriteMovie = (movie) => {
+        const newFavoriteList = [...favorites, movie];
+        setFavorites(newFavoriteList);
+    };
+
     useEffect(() => {
-        getMovieRequest();
-    }, []);
+        getMovieRequest(searchValue);
+    }, [searchValue]);
 	
 	return (
 		<div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Movies'/>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
 			<div className='row'>
-                <MovieList movies={movies} />
+                <MovieList 
+                    movies={movies} 
+                    favoriteComponent={AddFavorites}
+                    handleFavoriteClick={AddFavoriteMovie} 
+                />
 			</div>
 		</div>
+
+        //line 35 we are passing AddFavorite component as a prop(favoriteComponent)
 	);
 };
 
