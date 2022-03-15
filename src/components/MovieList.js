1e52d9e07d71058a64c840acc25dbf4244167a48
GloriaBargoria <bargoriagloria@gmail.com>
2022-03-15 16:52:00 +0300

    movie list update when favorites are added

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,23 @@
-import React from "react";
+import React from 'react';
 
-
-//pass list in movies as props and use map fn to loop through movies
 const MovieList = (props) => {
-    //take favoriteComponent prop and declare it as a variable and render it in overlay(line 14)
-    const FavouriteComponent = props.favouriteComponent;
-    return (
-        <>
-            {props.movies.map((movie, index) => (
-                <div className='image-container d-flex justify-content-start m-3'>
-                    <img src={movie.Poster} alt='movie'></img>
-                    <div 
-                        onClick={() => props.handleFavouritesClick(movie)}
-                        className='overlay d-flex align-items-center justify-content-center'
-                    >
+	const FavouriteComponent = props.favouriteComponent;
+
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container d-flex justify-content-start m-3'>
+					<img src={movie.Poster} alt='movie'></img>
+					<div
+						onClick={() => props.handleFavouritesClick(movie)}
+						className='overlay d-flex align-items-center justify-content-center'
+					>
 						<FavouriteComponent />
 					</div>
-                </div>
-            ))}
-        </>
-    );
+				</div>
+			))}
+		</>
+	);
 };
 
 export default MovieList;
